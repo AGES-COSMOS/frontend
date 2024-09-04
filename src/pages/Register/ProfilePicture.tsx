2e1321deb5c7821afe56a85344f3a957f32da1d4
guilhermedsa <gdsa.2k01@gmail.com>
2024-09-04 02:56:27 -0300

    adicionando opção de troca de imagem, dados mockados para inputs e finalização dos layouts de cadastro. To do: alinhar scss e criar user modificavel

--- src/pages/Register/ProfilePicture.tsx
+++ src/pages/Register/ProfilePicture.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import './ProfilePicture.scss';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 const ProfilePicture: React.FC = () => {
   // Cria uma referência para o input
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  // Estado para armazenar a URL da imagem
+  const [imageSrc, setImageSrc] = useState<string | ArrayBuffer | null>(null);
 
   // Função que é chamada quando a div é clicada
   const handleClick = () => {
@@ -13,18 +16,37 @@ const ProfilePicture: React.FC = () => {
     }
   };
 
+  // Função para lidar com a mudança do input de arquivo
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImageSrc(reader.result); // Define a URL da imagem no estado
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div className="profilePictureImg" onClick={handleClick}>
-      <input
-        type="file"
-        id="avatar"
-        name="avatar"
-        accept="image/png, image/jpeg"
-        ref={fileInputRef} // Adiciona a referência ao input
-        style={{ display: 'none' }} // Oculta o input file
-      />
-      {/* Opcional: Pode adicionar algum conteúdo visual ou texto aqui */}
-    </div>
+    <>
+      <div className="profilePictureEdit" onClick={handleClick}>
+        <input
+          type="file"
+          id="avatar"
+          name="avatar"
+          accept="image/png, image/jpeg"
+          ref={fileInputRef} // Referência ao input
+          style={{ display: 'none' }}
+          onChange={handleFileChange} // Adiciona o handler de mudança
+        />
+      </div>
+      <div
+        className="profilePictureImg"
+        style={{ backgroundImage: `url(${imageSrc})` }} // Define a imagem de fundo
+      ></div>
+    </>
   );
 };
+
 export default ProfilePicture;
