05a9cbf20ff744d0127ab5d544acdac685dd5abd
guilhermedsa <gdsa.2k01@gmail.com>
2024-09-04 02:56:27 -0300

    adicionando opção de troca de imagem, dados mockados para inputs e finalização dos layouts de cadastro. To do: alinhar scss e criar user modificavel

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -8,9 +8,29 @@ interface Option {
   label: string;
 }
 
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  cnpj?: string;
+  cpf?: string;
+  phone: string;
+  institution: string;
+  state: string;
+  city: string;
+  address: string;
+  address_number: number;
+}
+
 interface Instituicoes {
   value: string;
   label: string;
+}
+
+interface Estado {
+  value: string;
+  label: string;
+  city: string[];
 }
 
 const instituicoesOptions: Instituicoes[] = [
@@ -25,23 +45,50 @@ const selectOptions1: Option[] = [
   { value: 'Instituição', label: 'Instituição' },
 ];
 
-const selectOptions2: Option[] = [
-  { value: 'suboption1', label: 'Suboption 1' },
-  { value: 'suboption2', label: 'Suboption 2' },
+const selectEstado: Estado[] = [
+  {
+    value: 'rio_grande_do_sul',
+    label: 'Rio Grande do Sul',
+    city: ['Porto Alegre'],
+  },
+  { value: 'sao_paulo', label: 'São Paulo', city: ['São Paulo'] },
+  { value: 'minas_gerais', label: 'Minas Gerais', city: ['Belo Horizonte'] },
+  { value: 'santa_catarina', label: 'Santa Catarina', city: ['Florianópolis'] },
 ];
 
 const Register: React.FC = () => {
   const [selectedOption1, setSelectedOption1] = useState<string>('');
-  const [selectedOption2, setSelectedOption2] = useState<string>('');
+  const [selectedEstado, setSelectedEstado] = useState<string>('');
+  const [selectedCidade, setSelectedCidade] = useState<string>('');
   const [selectInstituicoesOptions, setSelectedInstituicaoOption] =
     useState<string>('');
+  const [cidades, setCidades] = useState<Option[]>([]);
 
   const handleSelectChange1 = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedOption1(event.target.value);
   };
 
-  const handleSelectChange2 = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOption2(event.target.value);
+  const handleSelectChangeEstado = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    const estadoValue = event.target.value;
+    setSelectedEstado(estadoValue);
+
+    // Filtra as cidades com base no estado selecionado
+    const estado = selectEstado.find((e) => e.value === estadoValue);
+    if (estado) {
+      setCidades(estado.city.map((city) => ({ value: city, label: city })));
+      setSelectedCidade(''); // Limpa a cidade selecionada quando o estado muda
+    } else {
+      setCidades([]);
+      setSelectedCidade('');
+    }
+  };
+
+  const handleSelectChangeCidade = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setSelectedCidade(event.target.value);
   };
 
   const handleSelectInstituicaoOption = (
@@ -53,16 +100,18 @@ const Register: React.FC = () => {
   return (
     <div className="container">
       <h1 className="title">Preencha todos os Campos</h1>
-      {/* CHOOSE THE TYPE OF USER  */}
+      <br />
       <ProfilePicture />
-
+      {/* CHOOSE THE TYPE OF USER  */}
       <div>
+        <br />
         <select
+          className="select-type-user"
           id="Cadastro"
           value={selectedOption1}
           onChange={handleSelectChange1}
         >
-          <option value=""></option>
+          <option value="">User</option>
           {selectOptions1.map((option) => (
             <option key={option.value} value={option.value}>
               {option.label}
@@ -70,91 +119,217 @@ const Register: React.FC = () => {
           ))}
         </select>
       </div>
-
       {/* LAYOUT FOR "PROFESSOR" USER */}
-
       {selectedOption1 === 'professor' && (
         <>
-          <input type="text" placeholder="Nome Completo" />
-          <input type="password" placeholder="Senha" />
-          <input type="password" placeholder="Confirme sua Senha" />
-          <input type="text" placeholder="CPF" />
-          <input type="email" placeholder="E-mail" />
-          <input
-            type="tel"
-            id="phoneProfessor"
-            name="phone"
-            placeholder="Telefone"
-            pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}"
-            required
-          />
-        </>
-      )}
-
-      {/* LAYOUT FOR "ALUNO" USER */}
-
-      {selectedOption1 === 'aluno' && (
-        <>
-          <div className="inputs-aluno">
+          <div className="input-container">
             <input type="text" placeholder="Nome Completo" />
+            <input className="email-input" type="email" placeholder="E-mail" />
+          </div>
+          <div className="input-container">
             <input type="password" placeholder="Senha" />
             <input type="password" placeholder="Confirme sua Senha" />
-            <input type="text" placeholder="CPF" />
-            <input type="email" placeholder="E-mail" />
-            <input
-              type="tel"
-              id="phoneAluno"
-              name="phone"
-              placeholder="Telefone"
-              pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}"
-              required
-            />
           </div>
-          {/* SELECT "INSTITUICAO" */}
           <div>
+            <div className="input-container">
+              <input type="text" placeholder="CPF" />
+              <select
+                className="select-type-instituicao"
+                id="Instituicoes"
+                value={selectInstituicoesOptions}
+                onChange={handleSelectInstituicaoOption}
+              >
+                <option value="">Instituição</option>
+                {instituicoesOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="input-container">
             <select
-              id="Instituicoes"
-              value={selectInstituicoesOptions}
-              onChange={handleSelectInstituicaoOption}
+              className="select-type-instituicao"
+              id="Estados"
+              value={selectedEstado}
+              onChange={handleSelectChangeEstado}
             >
-              <option value="">Instituicao</option>
-              {instituicoesOptions.map((option) => (
+              <option value="">Estado</option>
+              {selectEstado.map((option) => (
                 <option key={option.value} value={option.value}>
                   {option.label}
                 </option>
               ))}
             </select>
+            <select
+              className="select-type-instituicao"
+              id="Cidades"
+              value={selectedCidade}
+              onChange={handleSelectChangeCidade}
+            >
+              <option value="">Cidade</option>
+              {cidades.map((city) => (
+                <option key={city.value} value={city.value}>
+                  {city.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="input-container">
+            <input
+              className="input-endereco"
+              type="text"
+              placeholder="Endereço"
+            />
+            <input className="input-numero" type="text" placeholder="Número" />
           </div>
         </>
       )}
-
+      {/* LAYOUT FOR "ALUNO" USER */}
+      {selectedOption1 === 'aluno' && (
+        <>
+          <div className="input-container">
+            <input type="text" placeholder="Nome Completo" />
+            <input className="email-input" type="email" placeholder="E-mail" />
+          </div>
+          <div className="input-container">
+            <input type="password" placeholder="Senha" />
+            <input type="password" placeholder="Confirme sua Senha" />
+          </div>
+          <div>
+            <div className="input-container">
+              <input type="text" placeholder="CPF" />
+              <select
+                className="select-type-instituicao"
+                id="Instituicoes"
+                value={selectInstituicoesOptions}
+                onChange={handleSelectInstituicaoOption}
+              >
+                <option value="">Instituição</option>
+                {instituicoesOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="input-container">
+            <select
+              className="select-type-instituicao"
+              id="Estados"
+              value={selectedEstado}
+              onChange={handleSelectChangeEstado}
+            >
+              <option value="">Estado</option>
+              {selectEstado.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+            <select
+              className="select-type-instituicao"
+              id="Cidades"
+              value={selectedCidade}
+              onChange={handleSelectChangeCidade}
+            >
+              <option value="">Cidade</option>
+              {cidades.map((city) => (
+                <option key={city.value} value={city.value}>
+                  {city.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="input-container">
+            <input
+              className="input-endereco"
+              type="text"
+              placeholder="Endereço"
+            />
+            <input className="input-numero" type="text" placeholder="Número" />
+          </div>
+        </>
+      )}
       {/* LAYOUT FOR "INSTITUICAO" USER */}
-
       {selectedOption1 === 'Instituição' && (
         <>
-          <input type="text" placeholder="Nome Completo" />
-          <input type="password" placeholder="Senha" />
-          <input type="password" placeholder="Confirme sua Senha" />
-          <input type="text" placeholder="CNPJ" />
-          <input type="email" placeholder="E-mail" />
-          <input
-            type="tel"
-            id="phoneInstituicao"
-            name="phone"
-            placeholder="Telefone"
-            pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}"
-            required
-          />
+          <div className="input-container">
+            <input type="text" placeholder="Nome Completo" />
+            <input className="email-input" type="email" placeholder="E-mail" />
+          </div>
+          <div className="input-container">
+            <input type="password" placeholder="Senha" />
+            <input type="password" placeholder="Confirme sua Senha" />
+          </div>
+          <div>
+            <div className="input-container">
+              <input type="text" placeholder="CNPJ" />
+              <select
+                className="select-type-instituicao"
+                id="Instituicoes"
+                value={selectInstituicoesOptions}
+                onChange={handleSelectInstituicaoOption}
+              >
+                <option value="">Instituição</option>
+                {instituicoesOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="input-container">
+            <select
+              className="select-type-instituicao"
+              id="Estados"
+              value={selectedEstado}
+              onChange={handleSelectChangeEstado}
+            >
+              <option value="">Estado</option>
+              {selectEstado.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+            <select
+              className="select-type-instituicao"
+              id="Cidades"
+              value={selectedCidade}
+              onChange={handleSelectChangeCidade}
+            >
+              <option value="">Cidade</option>
+              {cidades.map((city) => (
+                <option key={city.value} value={city.value}>
+                  {city.label}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="input-container">
+            <input
+              className="input-endereco"
+              type="text"
+              placeholder="Endereço"
+            />
+            <input className="input-numero" type="text" placeholder="Número" />
+          </div>
         </>
       )}
       <>
         <div>
-          <button className="button-cancelar" type="submit">
-            CANCELAR
-          </button>
-          <button className="button-confirmar" type="submit">
-            CONFIRMAR
-          </button>
+          <div className="button-container">
+            <button className="button-cancelar" type="button">
+              CANCELAR
+            </button>
+            <button className="button-confirmar" type="submit">
+              CONFIRMAR
+            </button>
+          </div>
         </div>
       </>
     </div>
