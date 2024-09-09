ffc126ecd2219c68d602a33e58a7d8c866368086
ana.lopes <ana.lopes011@edu.pucrs.br>
2024-09-09 18:59:07 -0300

    Integração autors: Lucas, Nicoli

--- src/services/aboutUsService.ts
+++ src/services/aboutUsService.ts
@@ -2,7 +2,18 @@ import { httpService } from './httpService';
 
 export const getAboutUs = async () => {
   try {
-    const response = await httpService.get('/about-us');
+    const response = await getParameter('sobre');
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching about us data');
+  }
+};
+
+export const getParameter = async (param: string) => {
+  try {
+    const response = await httpService.get(
+      `/general-parameters/parameter/${param}`,
+    );
     return response.data;
   } catch (error) {
     throw new Error('Error fetching about us data');
