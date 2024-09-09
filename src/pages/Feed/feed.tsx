c892b7cf2be13821c44a59851ba30f11ef7b44da
lucamandelli <luca.mandelli@poatek.com>
2024-09-09 18:48:42 -0300

    added profilePhoto to projects mock and added overflow to feed css

--- src/pages/Feed/feed.tsx
+++ src/pages/Feed/feed.tsx
@@ -2,6 +2,8 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import './feed.scss';
 import { EventCard } from 'components/EventsCard/EventCard';
+import { Box } from '@mui/material';
+// import { ProjectCard } from 'components/ProjectCards/projectCard';
 
 const events = [
   {
@@ -39,35 +41,40 @@ const events = [
 const projects = [
   {
     title: 'Projeto 1',
-    photo: 'Here goes the photo',
+    projectPhoto: 'Here goes the projectPhoto',
+    profilePhoto: 'Here goes the profilePhoto',
     description: 'Descrição do projeto 1',
   },
   {
     title: 'Projeto 2',
-    photo: 'Here goes the photo',
+    projectPhoto: 'Here goes the projectPhoto',
+    profilePhoto: 'Here goes the profilePhoto',
     description: 'Descrição do projeto 2',
   },
   {
     title: 'Projeto 3',
-    photo: 'Here goes the photo',
+    projectPhoto: 'Here goes the projectPhoto',
+    profilePhoto: 'Here goes the profilePhoto',
     description: 'Descrição do projeto 3',
   },
   {
     title: 'Projeto 4',
-    photo: 'Here goes the photo',
+    projectPhoto: 'Here goes the projectPhoto',
+    profilePhoto: 'Here goes the profilePhoto',
     description: 'Descrição do projeto 4',
   },
   {
     title: 'Projeto 5',
-    photo: 'Here goes the photo',
+    projectPhoto: 'Here goes the projectPhoto',
+    profilePhoto: 'Here goes the profilePhoto',
     description: 'Descrição do projeto 5',
   },
 ];
 
 export const Feed = () => {
   return (
-    <div className="feed-container">
-      <div>
+    <Box className="feed-container">
+      <Box>
         <h2 className="feed-titles">Eventos</h2>
         <Carousel
           responsive={{
@@ -108,49 +115,21 @@ export const Feed = () => {
             );
           })}
         </Carousel>
-      </div>
-      <div>
+      </Box>
+      <Box>
         <h2 className="feed-titles">Projetos</h2>
-        <Carousel
-          responsive={{
-            desktop: {
-              breakpoint: {
-                max: 3000,
-                min: 1024,
-              },
-              items: 3,
-              partialVisibilityGutter: 40,
-            },
-            mobile: {
-              breakpoint: {
-                max: 464,
-                min: 0,
-              },
-              items: 1,
-              partialVisibilityGutter: 30,
-            },
-            tablet: {
-              breakpoint: {
-                max: 1024,
-                min: 464,
-              },
-              items: 2,
-              partialVisibilityGutter: 30,
-            },
-          }}
-        >
-          {projects.map((projects, i) => {
-            return (
-              <EventCard
-                key={i}
-                title={projects.title}
-                photo={projects.photo}
-                description={projects.description}
-              />
-            );
-          })}
-        </Carousel>
-      </div>
-    </div>
+        {/* {projects.map((projects, i) => {
+          return (
+            <EventCard
+              key={i}
+              title={projects.title}
+              photo={projects.photo}
+              description={projects.description}
+            />
+          );
+        })} */}
+        {/* <ProjectCard /> */}
+      </Box>
+    </Box>
   );
 };
