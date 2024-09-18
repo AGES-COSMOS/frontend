ca06ef1d061eaf28456489b06def7a0f00806902
Gabriel Barbosa <gabrielcbarbosa001@gmail.com>
2024-09-18 18:58:03 -0300

    Adaptando componentes da página de projetos para a de eventos

--- src/pages/CreateEvents/createEvents.tsx
+++ src/pages/CreateEvents/createEvents.tsx
@@ -7,15 +7,14 @@ import { Select } from 'components/Select/select';
 
 export interface Event {
   id: number;
-  name: string;
-  history: string;
-  purpose: string;
-  contact?: string;
-  start_date: Date;
-  end_date?: Date;
-  status: string;
-  teacher_id: number;
+  title: string;
+  description: string;
+  date: Date;
+  hour: Date;
+  IsOnline: boolean;
+  address: String;
   institution_id: number;
+  project_id: number;
   updatedAt: Date;
   updatedBy: string;
 }
@@ -26,9 +25,9 @@ export const CreateEvents = () => {
     { id: 2, name: 'UFRGS' },
   ]);
 
-  const [teachers] = useState([
-    { id: 1, name: 'Jorge Batista' },
-    { id: 2, name: 'Maria Silva' },
+  const [modality] = useState([
+    { id: 1, name: 'Online' },
+    { id: 2, name: 'Presencial' },
   ]);
 
   const [categories] = useState([
@@ -36,25 +35,18 @@ export const CreateEvents = () => {
     { id: 2, name: 'Direito Penal' },
   ]);
 
-  const [keywords] = useState([
-    { id: 1, name: 'Civil' },
-    { id: 2, name: 'Direito' },
-  ]);
-
   const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
-  const [selectedKeywords, setSelectedKeywords] = useState<number[]>([]);
   const [image, setImage] = useState('/assets/projectPlaceholder.png');
 
   const [eventData, setEventData] = useState<Event>({
     id: 0,
-    name: '',
-    history: '',
-    purpose: '',
-    contact: '',
-    start_date: new Date(),
-    end_date: undefined,
-    status: '',
-    teacher_id: 0,
+    title: '',
+    description: '',
+    address: '',
+    IsOnline: false,
+    date: new Date(),
+    hour: new Date(),
+    project_id: 0,
     institution_id: 0,
     updatedAt: new Date(),
     updatedBy: '',
@@ -65,13 +57,13 @@ export const CreateEvents = () => {
       HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
     >,
   ) => {
-    const { name, value } = e.target;
-    setEventData({ ...eventData, [name]: value });
+    const { title, value } = e.target;
+    setEventData({ ...eventData, [title]: value });
 
-    if (name === 'start_date' || name === 'end_date') {
-      setEventData({ ...eventData, [name]: new Date(value) });
+    if (title === 'date' || title === 'hour') {
+      setEventData({ ...eventData, [title]: new Date(value) });
     } else {
-      setEventData({ ...eventData, [name]: value });
+      setEventData({ ...eventData, [title]: value });
     }
   };
 
@@ -91,22 +83,11 @@ export const CreateEvents = () => {
     setEventData({ ...eventData, institution_id: institution?.id || 0 });
   };
 
-  const handleTeacherChange = (value: string) => {
-    const teacher = teachers.find((teacher) => teacher.name === value);
-    setEventData({ ...eventData, teacher_id: teacher?.id || 0 });
-  };
-
-  const handleStatusChange = (value: string) => {
-    setEventData({ ...eventData, status: value });
-  };
-
   const categoryOptions = categories.map((category) => category.name);
-  const keywordOptions = keywords.map((keyword) => keyword.name);
+  const modalityOptions = modality.map((modality) => modality.name);
   const institutionOptions = institutions.map(
     (institution) => institution.name,
   );
-  const teacherOptions = teachers.map((teacher) => teacher.name);
-  const statusOptions = ['Em Andamento', 'Finalizado'];
 
   const handleCategoryChange = (value: string) => {
     const selected = categories
@@ -115,20 +96,12 @@ export const CreateEvents = () => {
     setSelectedCategories(selected);
   };
 
-  const handleKeywordChange = (value: string) => {
-    const selected = keywords
-      .filter((keyword) => value.includes(keyword.name))
-      .map((keyword) => keyword.id);
-    setSelectedKeywords(selected);
-  };
-
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       const eventPayload = {
         ...eventData,
         categories: selectedCategories,
-        keywords: selectedKeywords,
       };
       await createEvent(eventPayload);
       alert('Evento criado com sucesso!');
@@ -138,13 +111,13 @@ export const CreateEvents = () => {
   };
 
   return (
-    <div className="create-project">
-      <h1>Cadastro de Projeto</h1>
+    <div className="create-event">
+      <h1>Cadastro de Evento</h1>
       <div className="image-container">
         <img
           src={image}
-          alt="Imagem de Capa do Projeto"
-          className="project-image"
+          alt="Imagem de Capa do Evento"
+          className="event-image"
         />
         <div className="add-image-button">
           <ButtonComponent
@@ -165,11 +138,11 @@ export const CreateEvents = () => {
       />
       <form className="form" onSubmit={handleSubmit}>
         <TextField
-          label="Nome do Projeto"
-          placeholder="Digite o nome do projeto"
+          label="Nome do Evento"
+          placeholder="Digite o título do evento"
           required
-          value={eventData.name}
-          onChange={(value) => setEventData({ ...eventData, name: value })}
+          value={eventData.title}
+          onChange={(value) => setEventData({ ...eventData, title: value })}
         />
 
         <Select
@@ -185,47 +158,39 @@ export const CreateEvents = () => {
         />
 
         <div>
-          <h4>Data de Início</h4>
+          <h4>Data do Evento</h4>
           <input
             type="date"
             className="item"
             name="start_date"
-            value={eventData.start_date.toISOString().split('T')[0]}
+            value={eventData.date.toISOString().split('T')[0]}
             onChange={handleInputChange}
             required
           />
         </div>
         <div>
-          <h4>Data de Fim</h4>
+          <h4>Horário do Evento</h4>
           <input
             type="date"
             className="item"
             name="end_date"
-            value={eventData.end_date?.toISOString().split('T')[0] || ''}
+            value={eventData.hour?.toISOString().split('T')[0] || ''}
             onChange={handleInputChange}
           />
         </div>
 
-        <Select
-          options={teacherOptions}
-          label="Professor"
-          placeholder="Selecione o Professor"
-          value={
-            teachers.find((teacher) => teacher.id === eventData.teacher_id)
-              ?.name
-          }
-          onChange={handleTeacherChange}
-          required
-        />
-
-        <Select
-          options={statusOptions}
-          label="Status do Projeto"
-          placeholder="Selecione o Status"
-          value={eventData.status}
-          onChange={handleStatusChange}
-          required
-        />
+        {/* <Select
+          options={modalityOptions}
+          label="Modalidade"
+          placeholder="Selecione a Modalidade"
+          value={modalityOptions
+            .map(
+              (id) =>
+                modality.find((modality) => modality.id === id)?.name || '',
+            )
+            .join(', ')}
+          onChange={handleCategoryChange}
+        /> */}
 
         <Select
           options={categoryOptions}
@@ -240,32 +205,12 @@ export const CreateEvents = () => {
           onChange={handleCategoryChange}
         />
 
-        <Select
-          options={keywordOptions}
-          label="Palavras-Chave"
-          placeholder="Selecione as Palavras-Chave"
-          value={selectedKeywords
-            .map(
-              (id) => keywords.find((keyword) => keyword.id === id)?.name || '',
-            )
-            .join(', ')}
-          onChange={handleKeywordChange}
-        />
-
         <textarea
-          name="history"
+          name="description"
           className="large-textarea"
-          value={eventData.history}
+          value={eventData.description}
           onChange={handleInputChange}
-          placeholder="História"
-          required
-        />
-        <textarea
-          name="purpose"
-          className="large-textarea"
-          value={eventData.purpose}
-          onChange={handleInputChange}
-          placeholder="Propósito"
+          placeholder="Descrição"
           required
         />
         <ButtonComponent
@@ -275,7 +220,7 @@ export const CreateEvents = () => {
           }}
           size={2}
         >
-          Cadastrar Projeto
+          Cadastrar Evento
         </ButtonComponent>
       </form>
     </div>
