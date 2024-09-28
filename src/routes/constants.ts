39e57bf94873c82c6da2e8d0a61bde5422189817
Guilherme Alves <g.alves001@edu.pucrs.br>
2024-09-28 09:43:15 -0300

    Adding the generic inputs, validations and some style.

--- src/routes/constants.ts
+++ src/routes/constants.ts
@@ -2,9 +2,6 @@ export const ROUTES = {
   FEED: () => '/',
   ABOUT_US: () => '/sobre-nos',
   PROJECT_LISTING: () => '/projetos',
-<<<<<<< HEAD
   REGISTER: () => '/cadastro',
-=======
   CREATE_PROJECTS: () => '/criar-projeto',
->>>>>>> 154ae93e0ee57e8c1b847509f4462a5955f5938f
 };
