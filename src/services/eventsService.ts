b6fd463d0cb84665e6f7472d9bd36f80a63b61e3
gabrielcbarbosa01 <gabrielcbarbosa001@gmail.com>
2024-09-28 18:14:56 -0300

    Correção do eventService

--- src/services/eventsService.ts
+++ src/services/eventsService.ts
@@ -3,7 +3,7 @@ import { Event } from '../pages/CreateEvents/createEvents';
 
 export const createEvent = async (eventData: Event): Promise<Event> => {
   try {
-    const response = await httpService.post('/projects', eventData);
+    const response = await httpService.post('/events', eventData);
     return response.data;
   } catch (error) {
     throw new Error('Erro ao criar evento.');
